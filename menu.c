e3ea821e11bc9aa120ff9bda9eb1abe3de78d7b8
Anca Stanoev <150381628+ancastanoev@users.noreply.github.com>
2024-07-08 21:38:27 +0300

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,20 @@
+#include "menu.h"
+#include <stdio.h>
+
+void menu() {
+    printf("Menu:\n");
+    printf("1. Add a book.\n");
+    printf("2. Search for a book.\n");
+    printf("3. Borrow a book.\n");
+    printf("4. Return a book.\n");
+    printf("5. Display all books.\n");
+    printf("6. View Loans.\n");
+    printf("7. Exit.\n");
+}
+
+
+void exitlib()
+{
+    printf("Goodbye!");
+
+}
